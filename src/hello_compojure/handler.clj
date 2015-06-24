2e1e7d8e7c5856eeaf85a511944e212d08e22800
Ryan Lewis Waudby <ryan@ryanwaudby.co.uk>
2015-06-24 14:58:56 +0100

    return 400 if sport is unknown and do not process sport data

--- src/hello_compojure/handler.clj
+++ src/hello_compojure/handler.clj
@@ -10,6 +10,9 @@
 (def known-sports
   {:tennis "tennis"})
 
+(defn valid-sport? [sport-id]
+  (contains? known-sports (keyword sport-id)))
+
 (def full-priority-order-url
   "/Users/waudbr01/repos/hello-clojure/full-priority-order.xml")
 
@@ -25,11 +28,16 @@
          :name (attr :full)))
 
 (defn sport-data-body [sport-id]
-  {:id (get known-sports (keyword sport-id) "unknown sport")
+  {:id (get known-sports (keyword sport-id))
    :events (tournament-titles sports-data-file)})
 
+(def invalid-request {:status 400 :body { :message "unknown sport" }})
+
 (defn sport-data [sport-id]
-  {:status 200 :body (sport-data-body sport-id)})
+  (if (valid-sport? sport-id)
+    {:status 200
+     :body (sport-data-body sport-id)}
+    invalid-request))
 
 (defroutes app-routes
   (GET "/statistics/:sport-id" [sport-id] (sport-data sport-id))
