37153ed403ea717ac688f139419b0c2ce5412e67
Ryan Lewis Waudby <ryan@ryanwaudby.co.uk>
2015-06-24 14:43:55 +0100

    refactored the main handler

--- src/hello_compojure/handler.clj
+++ src/hello_compojure/handler.clj
@@ -7,19 +7,32 @@
             [clojure.xml :as xml]
             [clojure.zip :as zip]))
 
-(def known-sports {:tennis "tennis" :football "soccer"})
+(def known-sports
+  {:tennis "tennis"})
 
-(defn tournament-titles [sports-data] (xml-> sports-data :tournament :tournament-metadata :name (attr :full)))
+(def full-priority-order-url
+  "/Users/waudbr01/repos/hello-clojure/full-priority-order.xml")
 
-(def sports-data-file (zip/xml-zip (xml/parse "/Users/waudbr01/repos/hello-clojure/full-priority-order.xml")))
+(defn parse-xml-feed [feed-url]
+  (zip/xml-zip (xml/parse feed-url)))
 
-(defn sport-body [sport-id] {:id (get known-sports (keyword sport-id) "unknown sport")
-                             :events (tournament-titles sports-data-file) })
+(def sports-data-file
+  (parse-xml-feed full-priority-order-url))
 
-(defn get-sport [sport-id] {:status 200 :body (sport-body sport-id) })
+(defn tournament-titles [sports-data]
+  (xml-> sports-data :tournament
+         :tournament-metadata
+         :name (attr :full)))
+
+(defn sport-data-body [sport-id]
+  {:id (get known-sports (keyword sport-id) "unknown sport")
+   :events (tournament-titles sports-data-file)})
+
+(defn sport-data [sport-id]
+  {:status 200 :body (sport-data-body sport-id)})
 
 (defroutes app-routes
-  (GET "/statistics/:sport-id" [sport-id] (get-sport sport-id))
+  (GET "/statistics/:sport-id" [sport-id] (sport-data sport-id))
   (route/not-found "Not Found"))
 
 (def app
